8756d7eedccd1ac3cc3b0eeb7d3482162466a85a
Tamás Gavrán <b0gavtam@vasvari.org>
2023-09-22 14:11:32 +0200

    nem mukodik

--- WarriorProject/Program.cs
+++ WarriorProject/Program.cs
@@ -3,14 +3,64 @@
  */
 
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 
 namespace WarriorProject
 {
+    public class Position
+    {
+        private int _x;
+        private int _y;
+
+        public int X { get => _x; }
+        public int Y { get => _y; }
+
+        public Position(int x, int y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+        public void ToConsole()
+        {
+            Console.WriteLine($"({X},{Y})");
+        }
+    }
+    public class Warrior
+    {
+        private Position p;
+        private string _name;
+        
+        public Warrior(string name, Position position) 
+        {
+            this._name = name;
+            this.p = position;
+        }
+        public void ToConsole()
+        {
+            Console.WriteLine($"{_name},{p}");
+        }
+        public void MoveHorizontal(int distance)
+        {
+            p.X += distance;
+        }
+        public void MoveVertical(int distance)
+        {
+            p.Y += distance;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position position = new Position(2, -1);
+            position.ToConsole();
+            Warrior warrior = new Warrior("Peter the Iron Man", position);
+            warrior.ToConsole();
+            warrior.MoveHorizontal(3);
+            warrior.MoveVertical(-2);
+            warrior.ToConsole();
         }
     }
 }
